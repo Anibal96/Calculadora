0c2db7a800615c5fd04fa6e9eb335d69b7407017
unknown <you@example.com>
2016-01-22 11:10:02 +0100

    Reduccion de codigo

--- Test.java
+++ Test.java
@@ -14,46 +14,60 @@ public class Test
         Calculadora calculadora = new Calculadora();
         boolean metodoCorrecto = true;
         if (calculadora.isPrime(2)){
-            System.out.println("Comprobando isPrime(2)... resultado correcto true / resultado metodo " + calculadora.isPrime(2) + " ... OK");
+            System.out.println("Comprobando isPrime(2)... resultado correcto true / resultado metodo true ... OK");
         }
         else{
-            System.out.println("Comprobando isPrime(2)... resultado correcto true / resultado metodo " + calculadora.isPrime(2) + " ... ERROR");
+            System.out.println("Comprobando isPrime(2)... resultado correcto true / resultado metodo false ... ERROR");
             metodoCorrecto = false;
         }
         if (calculadora.isPrime(4)){
-            System.out.println("Comprobando isPrime(4)... resultado correcto false / resultado metodo " + calculadora.isPrime(4) + " ... ERROR");
+            System.out.println("Comprobando isPrime(4)... resultado correcto false  / resultado metodo true ... ERROR");
             metodoCorrecto = false;
         }
         else{
-             System.out.println("Comprobando isPrime(4)... resultado correcto false / resultado metodo " + calculadora.isPrime(4) + " ... OK");
+             System.out.println("Comprobando isPrime(4)... resultado correcto false / resultado metodo false ... OK");
         }
         if (calculadora.isPrime(5)){
-            System.out.println("Comprobando isPrime(5)... resultado correcto true / resultado metodo " + calculadora.isPrime(5) + " ... OK");
+            System.out.println("Comprobando isPrime(5)... resultado correcto true / resultado metodo true ... OK");
         }
         else{
-            System.out.println("Comprobando isPrime(5)... resultado correcto true / resultado metodo " + calculadora.isPrime(5) + " ... ERROR");
+            System.out.println("Comprobando isPrime(5)... resultado correcto true / resultado metodo false ... ERROR");
             metodoCorrecto = false;
         }
         if (calculadora.isPrime(10)){
-            System.out.println("Comprobando isPrime(10)... resultado correcto false / resultado metodo " + calculadora.isPrime(10) + " ... ERROR");
+            System.out.println("Comprobando isPrime(10)... resultado correcto false / resultado metodo true ... ERROR");
             metodoCorrecto = false;
         }
         else{
-             System.out.println("Comprobando isPrime(10)... resultado correcto false / resultado metodo " + calculadora.isPrime(10) + " ... OK");
+             System.out.println("Comprobando isPrime(10)... resultado correcto false / resultado metodo false ... OK");
         }
         if (calculadora.isPrime(13)){
-            System.out.println("Comprobando isPrime(13)... resultado correcto true / resultado metodo " + calculadora.isPrime(13) + " ... OK");
+            System.out.println("Comprobando isPrime(13)... resultado correcto true / resultado metodo true ... OK");
         }
         else{
-            System.out.println("Comprobando isPrime(13)... resultado correcto true / resultado metodo " + calculadora.isPrime(13) + " ... ERROR");
+            System.out.println("Comprobando isPrime(13)... resultado correcto true / resultado metodo false ... ERROR");
             metodoCorrecto = false;
         }
         if (calculadora.isPrime(101)){
-            System.out.println("Comprobando isPrime(101)... resultado correcto true / resultado metodo " + calculadora.isPrime(101) + " ... OK");
+            System.out.println("Comprobando isPrime(101)... resultado correcto true / resultado metodo true ... OK");
         }
         else{
-            System.out.println("Comprobando isPrime(101)... resultado correcto true / resultado metodo " + calculadora.isPrime(101) + " ... ERROR");
+            System.out.println("Comprobando isPrime(101)... resultado correcto true / resultado metodo false ... ERROR");
             metodoCorrecto = false;
+        }
+        if (calculadora.isPrime(1)){
+            System.out.println("Comprobando isPrime(1)... resultado correcto false / resultado metodo true ... ERROR");
+            metodoCorrecto = false;
+        }
+        else{
+             System.out.println("Comprobando isPrime(1)... resultado correcto false / resultado metodo false ... OK");
+        }
+        if (calculadora.isPrime(-10)){
+            System.out.println("Comprobando isPrime(-10)... resultado correcto false / resultado metodo true ... ERROR");
+            metodoCorrecto = false;
+        }
+        else{
+             System.out.println("Comprobando isPrime(-10)... resultado correcto false / resultado metodo false ... OK");
         }
         if (metodoCorrecto){
             System.out.println("El método SI funciona correctamente");
@@ -71,45 +85,52 @@ public class Test
         Calculadora calculadora = new Calculadora();
         boolean metodoCorrecto = true;
         if (calculadora.sumValuesInterval(-1,8) == -1){
-            System.out.println("Comprobando sumValuesInterval(-1,8)...resultado correcto -1 / resultado metodo " + calculadora.sumValuesInterval(-1,8) + " ... OK");
+            System.out.println("Comprobando sumValuesInterval(-1,8)...resultado correcto -1 / resultado metodo -1 ... OK");
         }
         else{
             System.out.println("Comprobando sumValuesInterval(-1,8)...resultado correcto -1 / resultado metodo " + calculadora.sumValuesInterval(-1,8) + " ... ERROR");
             metodoCorrecto = false;
         }
         if (calculadora.sumValuesInterval(1,8) == 36){
-            System.out.println("Comprobando sumValuesInterval(1,8)...resultado correcto 36 / resultado metodo " + calculadora.sumValuesInterval(1,8) + " ... OK");
+            System.out.println("Comprobando sumValuesInterval(1,8)...resultado correcto 36 / resultado metodo 36 ... OK");
         }
         else{
             System.out.println("Comprobando sumValuesInterval(1,8)...resultado correcto 36 / resultado metodo " + calculadora.sumValuesInterval(1,8) + " ... ERROR");
             metodoCorrecto = false;
         }
         if (calculadora.sumValuesInterval(1,-8) == -1){
-            System.out.println("Comprobando sumValuesInterval(1,-8)...resultado correcto -1 / resultado metodo " + calculadora.sumValuesInterval(1,-8) + " ... OK");
+            System.out.println("Comprobando sumValuesInterval(1,-8)...resultado correcto -1 / resultado metodo -1 ... OK");
         }
         else{
             System.out.println("Comprobando sumValuesInterval(1,-8)...resultado correcto -1 / resultado metodo " + calculadora.sumValuesInterval(1,-8) + " ... ERROR");
             metodoCorrecto = false;
         }
         if (calculadora.sumValuesInterval(8,1) == 36){
-            System.out.println("Comprobando sumValuesInterval(8,1)...resultado correcto 36 / resultado metodo " + calculadora.sumValuesInterval(8,1) + " ... OK");
+            System.out.println("Comprobando sumValuesInterval(8,1)...resultado correcto 36 / resultado metodo 36 ... OK");
         }
         else{
             System.out.println("Comprobando sumValuesInterval(8,1)...resultado correcto 36 / resultado metodo " + calculadora.sumValuesInterval(8,1) + " ... ERROR");
             metodoCorrecto = false;
         }
         if (calculadora.sumValuesInterval(-1,-8) == -1){
-            System.out.println("Comprobando sumValuesInterval(-1,-8)...resultado correcto -1 / resultado metodo " + calculadora.sumValuesInterval(-1,-8) +  " ... OK");
+            System.out.println("Comprobando sumValuesInterval(-1,-8)...resultado correcto -1 / resultado metodo -1 ... OK");
         }
         else{
             System.out.println("Comprobando sumValuesInterval(-1,-8)...resultado correcto -1 / resultado metodo " + calculadora.sumValuesInterval(-1,-8) +  " ... ERROR");
             metodoCorrecto = false;
         }
         if (calculadora.sumValuesInterval(1,100) == 5050){
-            System.out.println("Comprobando sumValuesInterval(1,100)...resultado correcto 5050 / resultado metodo " + calculadora.sumValuesInterval(1,100) + " ... OK");
+            System.out.println("Comprobando sumValuesInterval(1,100)...resultado correcto 5050 / resultado metodo 5050 ... OK");
         }
         else{
             System.out.println("Comprobando sumValuesInterval(1,100)...resultado correcto 5050 / resultado metodo " + calculadora.sumValuesInterval(1,100) + " ... ERROR");
+            metodoCorrecto = false;
+        }
+        if (calculadora.sumValuesInterval(1,1) == 1){
+            System.out.println("Comprobando sumValuesInterval(1,1)...resultado correcto 1 / resultado metodo 1 ... OK");
+        }
+        else{
+            System.out.println("Comprobando sumValuesInterval(1,1)...resultado correcto 1 / resultado metodo " + calculadora.sumValuesInterval(1,1) + " ... ERROR");
             metodoCorrecto = false;
         }
         if (metodoCorrecto){
